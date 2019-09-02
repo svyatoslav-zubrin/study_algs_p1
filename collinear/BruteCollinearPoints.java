7151997b295fd23e7e4a69597877be44a61aece2
Slava Zubrin <szubrin79@gmail.com>
2019-09-02 07:39:11 +0300

    WIP on w2

---
+++ collinear/BruteCollinearPoints.java
@@ -0,0 +1,28 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class BruteCollinearPoints {
+
+    // finds all line segments containing 4 points
+    public BruteCollinearPoints(Point[] points) {
+        // todo: implementation needed
+    }
+
+    // the number of line segments
+    public int numberOfSegments() {
+        return 0; // todo: implementation needed
+    }
+
+    // the line segments
+    public LineSegment[] segments() {
+        LineSegment[] sgmts = {};
+        return sgmts; // todo: implementation needed
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
